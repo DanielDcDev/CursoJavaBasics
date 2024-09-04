b7b7ffc9209ce38112070999d08cb13ffcf24925
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2024-09-04 13:40:47 +0100

    banco

--- mavenproject1/src/main/java/Telas/cadastro.java
+++ mavenproject1/src/main/java/Telas/cadastro.java
@@ -1,4 +1,4 @@
-package com.mycompany.mavenproject1;
+package Telas;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -7,6 +7,8 @@ public class cadastro extends javax.swing.JFrame {
 
     public cadastro() {
         initComponents();
+        btnCadaster.setVisible(false);
+
     }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -14,9 +16,9 @@ public class cadastro extends javax.swing.JFrame {
 
         jLabel3 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        btnCadaster = new javax.swing.JButton();
         jPasswordField1 = new javax.swing.JPasswordField();
-        jTextField1 = new javax.swing.JTextField();
+        txtNome = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
@@ -25,6 +27,9 @@ public class cadastro extends javax.swing.JFrame {
         jTextField2 = new javax.swing.JTextField();
         SpMonth = new javax.swing.JSpinner();
         SpYear = new javax.swing.JSpinner();
+        jLabel7 = new javax.swing.JLabel();
+        txtNumID = new javax.swing.JTextField();
+        PBar = new javax.swing.JProgressBar();
 
         jLabel3.setText("jLabel3");
 
@@ -32,17 +37,31 @@ public class cadastro extends javax.swing.JFrame {
 
         jLabel1.setText("Rude Bank");
 
-        jButton1.setText("Cadastrar");
+        btnCadaster.setText("Cadastrar");
+        btnCadaster.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCadasterActionPerformed(evt);
+            }
+        });
 
         jPasswordField1.setText("jPasswordField1");
+        jPasswordField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jPasswordField1ActionPerformed(evt);
+            }
+        });
 
-        jTextField1.setText("jTextField1");
+        txtNome.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtNomeActionPerformed(evt);
+            }
+        });
 
         jLabel2.setText("Name");
 
-        jLabel4.setText("Login");
+        jLabel4.setText("Password");
 
-        jLabel5.setText("Age");
+        jLabel5.setText("Born ");
 
         btnAge.setText("verificar");
         btnAge.addActionListener(new java.awt.event.ActionListener() {
@@ -53,43 +72,61 @@ public class cadastro extends javax.swing.JFrame {
 
         jLabel6.setText("secret word");
 
+        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField2ActionPerformed(evt);
+            }
+        });
+
         SpMonth.setModel(new javax.swing.SpinnerNumberModel(1, 1, 31, 1));
 
         SpYear.setModel(new javax.swing.SpinnerNumberModel(2000, 1980, 2020, 1));
+
+        jLabel7.setText("CPF");
+
+        txtNumID.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtNumIDActionPerformed(evt);
+            }
+        });
+
+        PBar.setBackground(new java.awt.Color(0, 0, 0));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addGap(116, 116, 116)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel6)
+                    .addComponent(jLabel7))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(49, 49, 49)
-                        .addComponent(jLabel1)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel5)
-                            .addComponent(jLabel4)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel6))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addComponent(jTextField2)
-                                .addComponent(jPasswordField1)
-                                .addComponent(jTextField1))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(SpMonth, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(SpYear, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(SpMonth, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnAge))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(jButton1)))
-                .addContainerGap())
+                        .addComponent(SpYear, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jPasswordField1)
+                    .addComponent(txtNome)
+                    .addComponent(txtNumID, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jTextField2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnAge)
+                .addGap(13, 13, 13))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(btnCadaster, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(PBar, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addGap(19, 19, 19))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -98,7 +135,7 @@ public class cadastro extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addGap(37, 37, 37)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -114,9 +151,15 @@ public class cadastro extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel6)
                     .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 140, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(18, 18, 18))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel7)
+                    .addComponent(txtNumID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(PBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnCadaster)
+                .addContainerGap(38, Short.MAX_VALUE))
         );
 
         pack();
@@ -134,11 +177,39 @@ public class cadastro extends javax.swing.JFrame {
         int ida = (year- ano)-a;
         
         //logica para descobrir a idade exata por meio do mes/ano
-        
-        
-        btnAge.setText(Integer.toString(ida));
+        //disponibilizando o botao de cadastro, mas o que eu quero e que ele fique mais claro par que eu possa clicar
+        btnCadaster.setVisible(true);
         
     }//GEN-LAST:event_btnAgeActionPerformed
+
+    private void txtNomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNomeActionPerformed
+        // Aqui eu quero que quando o usuario terminar de colocar o nome dele 
+        //a barra de progresso atualize e fique mais proxima do final.
+        
+    }//GEN-LAST:event_txtNomeActionPerformed
+
+    private void txtNumIDActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNumIDActionPerformed
+// Aqui eu quero que quando o usuario terminar de colocar o nome dele 
+//a barra de progresso atualize e fique mais proxima do final.
+    }//GEN-LAST:event_txtNumIDActionPerformed
+
+    private void btnCadasterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadasterActionPerformed
+        // TODO add your handling code here:
+        Login l = new Login();
+        this.dispose();
+        l.setVisible(true);
+    }//GEN-LAST:event_btnCadasterActionPerformed
+
+    private void jPasswordField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordField1ActionPerformed
+        // Aqui eu quero que quando o usuario terminar de colocar o nome dele 
+        //a barra de progresso atualize e fique mais proxima do final.
+        // e eu quero um botao pra que quando eu clique os caracteres fiquem visiveis quando eu clique 
+    }//GEN-LAST:event_jPasswordField1ActionPerformed
+
+    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+        // Aqui eu quero que quando o usuario terminar de colocar o nome dele 
+        //a barra de progresso atualize e fique mais proxima do final.
+    }//GEN-LAST:event_jTextField2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -176,18 +247,21 @@ public class cadastro extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JProgressBar PBar;
     private javax.swing.JSpinner SpMonth;
     private javax.swing.JSpinner SpYear;
     private javax.swing.JButton btnAge;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btnCadaster;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField txtNome;
+    private javax.swing.JTextField txtNumID;
     // End of variables declaration//GEN-END:variables
 }
