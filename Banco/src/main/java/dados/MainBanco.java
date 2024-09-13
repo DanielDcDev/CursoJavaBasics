9cf50ecf7322bddd063403d0fa893395b12c655b
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2024-09-14 00:19:20 +0100

    correcao

---
+++ Banco/src/main/java/dados/MainBanco.java
@@ -0,0 +1,30 @@
+package dados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author daniel
+ */
+
+ public class MainBanco {
+    public static void main(String[] args) {
+        //atributos
+        String nome ;
+        String login;
+        Double anNasc;
+        Double depI;
+
+        //Scanner
+        Scanner tec = new Scanner(System.in);
+        //Scanner
+
+        System.out.print("//-Banco Rude-//"
+                          +"Nome: ");
+        tec.next(nome);
+
+
+
+    }
+}
